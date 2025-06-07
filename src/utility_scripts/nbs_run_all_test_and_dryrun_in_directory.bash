49c22e78162414dc0d935ccce19878903851f535
RedLeader962 <redleader962@gmail.com>
2025-06-07 10:40:09 -0400

    fix(scripts): ensure proper error handling and consistent formatting in `nbs_run_all_test_and_dryrun_in_directory.bash`
    - Integrated NorLab splash display for better script feedback.
    - Delete the console `clear` command so that stderr so that the script be usable during debugging sessions.
    - Move `target_dir_path` inside the function.
    - Added error validation for the directory parameter to prevent execution in invalid paths.
    - Standardized variable naming and applied consistent color-coded exit codes.
    
    Issue NMO-688

--- src/utility_scripts/nbs_run_all_test_and_dryrun_in_directory.bash
+++ src/utility_scripts/nbs_run_all_test_and_dryrun_in_directory.bash
@@ -1,77 +1,78 @@
 #!/bin/bash
-#
+# =================================================================================================
 # Run all bash script in a directory with name prefixed by "test_" or "dryrun_"
 #
 # Usage in a script:
 #
 #   #!/bin/bash
-#   _PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
-#   SCRIPT_DIR_PATH="$(dirname "${_PATH_TO_SCRIPT}")"
-#   NBS_UTIL_SCRIPT="${SCRIPT_DIR_PATH}/../../src/utility_scripts"
-#   source "${NBS_UTIL_SCRIPT}/nbs_run_all_test_and_dryrun_in_directory.bash" "$SCRIPT_DIR_PATH"
+#   path_to_script="$(realpath "${BASH_SOURCE[0]:-'.'}")"
+#   target_dir_path="$(dirname "${path_to_script}")"
+#   nbs_util_script="${target_dir_path}/../../src/utility_scripts"
+#   source "${nbs_util_script}/nbs_run_all_test_and_dryrun_in_directory.bash" "${target_dir_path}"
 #
-
-
-SCRIPT_DIR_PATH="${1:?'[ERROR] Missing mandatory path to directory to run script'}"
-
-MSG_DIMMED_FORMAT="\033[1;2m"
-MSG_ERROR_FORMAT="\033[1;31m"
-MSG_DONE_FORMAT="\033[1;32m"
-MSG_END_FORMAT="\033[0m"
+# Globals:
+#   none
+# =================================================================================================
 
 function nbs::run_all_script_in_directory(){
+  local tmp_cwd
+  tmp_cwd=$(pwd)
+
   set +e            # Propagate exit code ‹ Dont exit on error
   set +o pipefail   # Propagate exit code ‹ Dont exit if errors within pipes
 
-  local _TMP_CWD
-  _TMP_CWD=$(pwd)
-  cd "${SCRIPT_DIR_PATH}" || exit 1
+  local target_dir_path="$1"
+  if [[ ! -d ${target_dir_path}  ]]; then
+    n2st::print_msg_error_and_exit "test/dryrun directory is unreachable"
+  fi
+  cd "${target_dir_path:?err}" || exit 1
 
-  local OVERALL_EXIT_CODE=0
-  declare -a FILE_NAME
+  local overall_exit_code=0
+  declare -a file_name
 
   # ====Begin======================================================================================
+
   # ....Run bash script prefixed with "dryrun_"....................................................
-  for each_file in "${SCRIPT_DIR_PATH}"/dryrun_*.bash ; do
+  for each_file in "${target_dir_path}"/dryrun_*.bash ; do
     if [[ -f $each_file ]]; then
       bash "${each_file}"
-      EXIT_CODE=$?
-      if [[ ${EXIT_CODE} != 0 ]]; then
-          FILE_NAME+=( "${MSG_ERROR_FORMAT}   exit code $EXIT_CODE ‹ $( basename "${each_file}" )${MSG_END_FORMAT}")
-          OVERALL_EXIT_CODE=${EXIT_CODE}
+      exit_code=$?
+      if [[ ${exit_code} != 0 ]]; then
+          file_name+=( "${MSG_ERROR_FORMAT}   exit code $exit_code ‹ $( basename "${each_file}" )${MSG_END_FORMAT}")
+          overall_exit_code=${exit_code}
       else
-        FILE_NAME+=( "${MSG_DONE_FORMAT}   exit code $EXIT_CODE ‹ $( basename "${each_file}" )${MSG_END_FORMAT}")
+        file_name+=( "${MSG_DONE_FORMAT}   exit code $exit_code ‹ $( basename "${each_file}" )${MSG_END_FORMAT}")
       fi
     fi
   done
 
   # ....Run bash script prefixed with "test_"......................................................
-  for each_file in "${SCRIPT_DIR_PATH}"/test_*.bash ; do
+  for each_file in "${target_dir_path}"/test_*.bash ; do
     if [[ -f $each_file ]]; then
       bash "${each_file}"
-      EXIT_CODE=$?
-      if [[ ${EXIT_CODE} != 0 ]]; then
-          FILE_NAME+=( "${MSG_ERROR_FORMAT}   exit code $EXIT_CODE ‹ $( basename "${each_file}" )${MSG_END_FORMAT}")
-          OVERALL_EXIT_CODE=${EXIT_CODE}
+      exit_code=$?
+      if [[ ${exit_code} != 0 ]]; then
+          file_name+=( "${MSG_ERROR_FORMAT}   exit code $exit_code ‹ $( basename "${each_file}" )${MSG_END_FORMAT}")
+          overall_exit_code=${exit_code}
       else
-        FILE_NAME+=( "${MSG_DONE_FORMAT}   exit code $EXIT_CODE ‹ $( basename "${each_file}" )${MSG_END_FORMAT}")
+        file_name+=( "${MSG_DONE_FORMAT}   exit code $exit_code ‹ $( basename "${each_file}" )${MSG_END_FORMAT}")
       fi
     fi
   done
 
 
   # ....Show result................................................................................
-  clear
+  n2st::norlab_splash "${NBS_SPLASH_NAME_BUILD_SYSTEM:-NorLab-Build-System}" "https://github.com/norlab-ulaval/norlab-build-system.git"
 
-  echo -e "Results from ${MSG_DIMMED_FORMAT}$0${MSG_END_FORMAT} in directory ${MSG_DIMMED_FORMAT}$( basename "${SCRIPT_DIR_PATH}" )/${MSG_END_FORMAT} \n"
-  for each_file_run in "${FILE_NAME[@]}" ; do
+  echo -e "Results from ${MSG_DIMMED_FORMAT}$0${MSG_END_FORMAT} in directory ${MSG_DIMMED_FORMAT}$( basename "${target_dir_path}" )/${MSG_END_FORMAT} \n"
+  for each_file_run in "${file_name[@]}" ; do
       echo -e "$each_file_run"
   done
 
   # ====Teardown===================================================================================
-  cd "${_TMP_CWD}" || exit 1
+  cd "${tmp_cwd}" || exit 1
 
-  return $OVERALL_EXIT_CODE
+  return $overall_exit_code
 }
 
 # ::::Main:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -81,6 +82,10 @@ if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
   exit 1
 else
   # This script is being sourced, ie: __name__="__source__"
-  nbs::run_all_script_in_directory
+  script_path="$(realpath "${BASH_SOURCE[0]:-'.'}")"
+  script_path_parent="$(dirname "${script_path}")"
+  source "${script_path_parent}/../../import_norlab_build_system_lib.bash" || exit 1
+
+  nbs::run_all_script_in_directory "$@"
   exit $?
 fi
